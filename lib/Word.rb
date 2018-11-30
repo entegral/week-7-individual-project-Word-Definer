29316fb0dc1607f78d3f19744e98b7a0a65ef8ff
Rob <e2robby@gmail.com>
2018-11-30 09:53:10 -0800

    add basic-functionality class methods

--- lib/Word.rb
+++ lib/Word.rb
@@ -1,5 +1,37 @@
 class Word
+  @@word_list = []
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @definitions = []
+    add_definition(attributes)
+    @id = @@word_list.length + 1
+  end
+
+
+  def self.all
+    @@word_list
+  end
+
+
+  def self.find(id)
+    for each in @@word_list
+      if each.id == id.to_i
+        return each
+    end
+    "Word ID not found in word list."
+  end
+
+
+  def add_definition(attributes)
+    @definitions.push(attributes[:definition])
+  end
+
+
+  def save
+    @@word_list.push(self)
+  end
+
+
 
 end
-
-
